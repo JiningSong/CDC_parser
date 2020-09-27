0f66fc1603261de38fffbe70d9008508d7c59f3e
Jining Song <jiningsmail@gmail.com>
2020-09-26 20:44:12 -0700

    cleaned repo

--- parse_CDC_articles.py
+++ parse_CDC_articles.py
@@ -11,6 +11,7 @@ from util import generate_soup
 SEARCH_RESULTS_BASE_URL = "https://search.cdc.gov/search/index.html?any=%22drug%20use%22%20%22drug%20abuse%22&url=mmwr&date1=01%2F01%2F2016&dpage={}#content"
 MAX_PAGE = 108
 
+
 def get_search_result_urls():
     search_result_urls = []
     with tqdm(total=MAX_PAGE, desc="Crawling HAN Messages: ") as pbar:
@@ -24,19 +25,13 @@ def get_search_result_urls():
 
     return search_result_urls
 
+
 def read_list():
     f = open('search_result_urls.txt', 'r')
     search_result_urls = ast.literal_eval(f.read())
     f.close()
     return search_result_urls
 
-search_result_urls = read_list()
-
-# check if url contain html or htm. (only consider these cases)
-message_urls = []
-for url in search_result_urls:
-    if re.match("^https://www.cdc.gov/mmwr/volumes/.*(html|htm)$", url) is not None:
-        message_urls.append(url)
 
 # get title
 def get_message_title(soup):
@@ -44,19 +39,23 @@ def get_message_title(soup):
         title = soup.find('span', attrs={'class': 'mobile-title'}).get_text()
     except:
         title = 'null'
-    
+
     return title
 
 # get date
+
+
 def get_message_time(soup):
     try:
         date = str(soup.select_one('.dateline > p')).split('/')[2]
     except:
         date = 'null'
-    
+
     return date
 
 # get links within message body
+
+
 def get_links(soup):
     cdc_links = []
     try:
@@ -70,7 +69,7 @@ def get_links(soup):
                     '/contact' not in href.lower() and\
                     '/index' not in href.lower() and\
                     '/cdc-info' not in href.lower() and\
-                    '/about' not in href.lower():
+                        '/about' not in href.lower():
                     cdc_links.append(href)
             except:
                 pass
@@ -80,12 +79,15 @@ def get_links(soup):
     return cdc_links
 
 # get the entire message body
+
+
 def get_message_body(soup):
     full_text = ""
-    
+
     # finding upper gray area of text
     try:
-        gray_contents = soup.find('div', {"class": re.compile('.*order-0.*')}).find_all('p')
+        gray_contents = soup.find(
+            'div', {"class": re.compile('.*order-0.*')}).find_all('p')
         for elem in gray_contents:
             text = elem.get_text()
             text = re.sub('\(\d+[^)]*\)', '', text, 100)
@@ -97,7 +99,7 @@ def get_message_body(soup):
     # finding lower white area of text
     texts = []
     try:
-        contents = soup.findAll('div',attrs={"class":"order-4 w-100"})
+        contents = soup.findAll('div', attrs={"class": "order-4 w-100"})
 
         for elem in contents:
             children = elem.find_all(['div', 'p'], recursive=False)
@@ -118,20 +120,22 @@ def get_message_body(soup):
     return full_text
 
 # find title, date, text, links(consider: get_links from han_parser.py)
+
+
 def parse_data(url):
     soup = generate_soup(url)
 
     # Find message title
-    title = get_message_title(soup)      
+    title = get_message_title(soup)
     # Find message publish date
     publish_date = get_message_time(soup)
     # Find CDC links
     links = get_links(soup)
     # Find texts
     text = get_message_body(soup)
-    
+
     # result_dict = {
-    #     "message_id": message_id, 
+    #     "message_id": message_id,
     #     "title": title,
     #     "url": message_url,
     #     "message_type": message_type,
@@ -151,12 +155,20 @@ def parse_data(url):
 
 
 if __name__ == "__main__":
+    search_result_urls = read_list()
+
+    # check if url contain html or htm. (only consider these cases)
+    message_urls = []
+    for url in search_result_urls:
+        if re.match("^https://www.cdc.gov/mmwr/volumes/.*(html|htm)$", url) is not None:
+            message_urls.append(url)
+
     # Output result list
     results = []
-    
+
     # loops through all message_urls and store parsed data into results list as dicts
     with tqdm(total=len(message_urls), desc="Crawling search result Messages: ") as pbar:
-       
+
         for message_url in message_urls:
             result_dict = parse_data(message_url)
             results.append(result_dict)
--- util.py
+++ util.py
@@ -1,14 +1,20 @@
-from requests_html import HTMLSession
+# from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+import requests
 
+
+# def generate_soup(url):
+#     session = HTMLSession()
+#     try:
+#         r = session.get(url)
+#         r.html.render()
+
+#         if r.status_code == 200:
+#             return BeautifulSoup(r.html.html, "lxml")
+#     except Exception as e:
+#         print(e)
 
 def generate_soup(url):
-    session = HTMLSession()
-    try:
-        r = session.get(url)
-        r.html.render()
-
-        if r.status_code == 200:
-            return BeautifulSoup(r.html.html, "lxml")
-    except Exception as e:
-        print(e)
+    markup = requests.get(url).content
+    soup = BeautifulSoup(markup, "lxml")
+    return soup
